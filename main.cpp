fd1270a72f55826b5ecf94edc389a445ee9d9389
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-03-12 18:58:36 +0545

    apply clang-format

--- main.cpp
+++ main.cpp
@@ -3,107 +3,104 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
-#include <iostream>
 #include <cctype>
-#include <vector>
-#include <utility>
 #include <functional>
+#include <iostream>
+#include <utility>
+#include <vector>
 // todo: design a proper structure..
 // single variable function
- 
+
 class Function {
 private:
   std::function<double(int)> m_function;
   char m_variable;
 
   std::vector<std::string> m_params;
-  
+
 public:
   // no mechanism to confirm a single variable
-  Function(char* str) { 
-       assign_lamdda(str);
-  }
+  Function(char *str) { assign_lamdda(str); }
 
-  void assign_lamdda(char* str){
-    char* current = str;
-    char* next = str;
+  void assign_lamdda(char *str) {
+    char *current = str;
+    char *next = str;
 
-    while(*current != '\0'){
-      if(isspace(*current)){
+    while (*current != '\0') {
+      if (isspace(*current)) {
         next++;
-	current++;
-      } else if(isdigit(*current)){
-	   int num = int(*current) - 48;
-	  while(isdigit(*(++next))){
-	    num = num * 10 + (*next - 48); 
-	    current++;
-	  }
-	  current = next;
-	  m_params.emplace_back(std::to_string(num));
-      } else if (isalpha(*current)){
-	 // for future to check if its a single var or not
-	 m_variable = *current;
-	 //std::string temp(m_variable);
-	 m_params.emplace_back("x");
-	 current++;
-	 next++;
-      } else if ((*current) == '+'){
-	  m_params.emplace_back("+");
-	  current++;
-	  next++;
-      } else if((*current) == '-'){
-	 m_params.emplace_back("-");
-	 current++;
-	 next++;
-      } else if((*current) == '*'){
-	 m_params.emplace_back("*");
-	 current++;
-	 next++;
-      } else if((*current) == '/'){
-	 m_params.emplace_back("/");
-	 current++;
-	 next++;
+        current++;
+      } else if (isdigit(*current)) {
+        int num = int(*current) - 48;
+        while (isdigit(*(++next))) {
+          num = num * 10 + (*next - 48);
+          current++;
+        }
+        current = next;
+        m_params.emplace_back(std::to_string(num));
+      } else if (isalpha(*current)) {
+        // for future to check if its a single var or not
+        m_variable = *current;
+        // std::string temp(m_variable);
+        m_params.emplace_back("x");
+        current++;
+        next++;
+      } else if ((*current) == '+') {
+        m_params.emplace_back("+");
+        current++;
+        next++;
+      } else if ((*current) == '-') {
+        m_params.emplace_back("-");
+        current++;
+        next++;
+      } else if ((*current) == '*') {
+        m_params.emplace_back("*");
+        current++;
+        next++;
+      } else if ((*current) == '/') {
+        m_params.emplace_back("/");
+        current++;
+        next++;
       }
-
     }
-       m_function = [&](int x)-> double{
-	    double result = 0;
-	    char current_op = '*';
-	    bool no_op_read = true;
-	   for(const auto& elt : m_params){
-		if(isdigit(elt[0])){
-		  if(no_op_read)
-		    result =  std::stoi(elt);	
-		  else{
-		   result = perform_operation(result, std::stoi(elt), current_op);	  
-		  }
-		} else if (elt == "+") {
-                      current_op = '+';
-		      no_op_read = false;
-                } else if (elt == "-") {
-		      no_op_read = false;
-                      current_op = '-';
-                } else if (elt == "/") {
-		       no_op_read = false;
-                       current_op = '/';
-                } else if (elt == "*") {
-			no_op_read = false;
-                       current_op = '*';
-                } else if (isalpha(elt[0])) {
-	              if(no_op_read) result = 1;
-                      result = perform_operation(result, x, current_op);
-                }
-	   }
-	   return result;
-      };
+    m_function = [&](int x) -> double {
+      double result = 0;
+      char current_op = '*';
+      bool no_op_read = true;
+      for (const auto &elt : m_params) {
+        if (isdigit(elt[0])) {
+          if (no_op_read)
+            result = std::stoi(elt);
+          else {
+            result = perform_operation(result, std::stoi(elt), current_op);
+          }
+        } else if (elt == "+") {
+          current_op = '+';
+          no_op_read = false;
+        } else if (elt == "-") {
+          no_op_read = false;
+          current_op = '-';
+        } else if (elt == "/") {
+          no_op_read = false;
+          current_op = '/';
+        } else if (elt == "*") {
+          no_op_read = false;
+          current_op = '*';
+        } else if (isalpha(elt[0])) {
+          if (no_op_read)
+            result = 1;
+          result = perform_operation(result, x, current_op);
+        }
+      }
+      return result;
+    };
   }
 
-  double Evaluate(int input) {
-    return m_function(input);	  
-  }
+  double Evaluate(int input) { return m_function(input); }
 
-  void debug(){
-    for(const auto& elt: m_params) std::cout << elt << '\n';	  
+  void debug() {
+    for (const auto &elt : m_params)
+      std::cout << elt << '\n';
   }
   double perform_operation(const double &a, const double &b, const char op) {
     if (op == '+')
@@ -140,17 +137,15 @@ private:
 public:
   Plot() { sAppName = "Plot C++"; }
   // inititalization goes here
-  bool OnUserCreate() override {	 
-      SetPoints(Range(1, 390, 1), Function("5x"));
-       return true;
+  bool OnUserCreate() override {
+    SetPoints(Range(1, 390, 1), Function("5x"));
+    return true;
   }
 
   // this fn is called for every frame..
   bool OnUserUpdate(float fElpasedTime) override {
     Clear(olc::WHITE);
     DrawAxes();
-//    if (coordinates.size() == 0)
-     // SetPoints(Range(1, 390, 1), Function("x * x * x * x"));
     PlotPoints();
     return true;
   }
@@ -176,8 +171,8 @@ public:
   }
 };
 int main() {
-  
- Plot myPlot;
+
+  Plot myPlot;
   if (myPlot.Construct(400, 400, 4, 4)) {
     myPlot.Start();
   }
