1c7067e536a4b53bd03fc1321b18cb73899a3f22
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-03-16 14:46:23 +0545

    feat: add comments for example

--- example/main.cpp
+++ example/main.cpp
@@ -1,9 +1,13 @@
 #include "../plotter.h"
 
 int main() {
+  // define a new function.
   Function f("5x");
+  // define the range
   Range r(1,400,1);
+  // create a plot object with range and the function
   Plot myPlot(r,f);
+  // call the plot function to plot
   myPlot.plot();
   return 0;
 }
