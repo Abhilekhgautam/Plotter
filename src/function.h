b74c880752c23c9e0a4e2c013fc0f9bb319b9c1a
abhilekhgautam <abhilekhgautam119@gmail.com>
2024-03-25 00:23:23 +0545

    refactor: define a lexer fn

--- src/function.h
+++ src/function.h
@@ -1,96 +1,73 @@
 #pragma once
+
+#include <algorithm>
+#include <string_view>
 #include <cctype>
 #include <functional>
 #include <iostream>
+#include <map>
 #include <utility>
 #include <vector>
 
+bool IsOperator(char c){
+  if(c == '+' || c == '-' || c == '/' || c == '*' || c == '^' || c == '(' || c == ')') return true;
+  else return false;
+}
+
+template <char Var>
 class Function {
 private:
   // callable function 
   std::function<double(int)> m_function;
   char m_variable;
-
-  std::vector<std::string> m_params;
+  std::string m_function_str;
+  // 0 -> Variable
+  // 1 -> digit
+  // 2 -> op
+  // 3 -> space
+  std::map<char, int> m_lookup_table;
 
 public:
-  Function() = default;
   // no mechanism to confirm a single variable
-  Function(char *str) { assign_lamdda(str); }
+  Function(std::string str) {
+    m_function_str = str;
+    lexer(); 
+  }
 
   // todo: look a way with modern C++.. 
-  void assign_lamdda(char *str) {
-    char *current = str;
-    char *next = str;
-    // error prone .. walk carefully
-    while (*current != '\0') {
-      if (isspace(*current)) {
-        next++;
-        current++;
-      } else if (isdigit(*current)) {
-        int num = int(*current) - 48;
-        while (isdigit(*(++next))) {
-          num = num * 10 + (*next - 48);
-          current++;
-        }
-        current = next;
-        m_params.emplace_back(std::to_string(num));
-      } else if (isalpha(*current)) {
-        // for future to check if its a single var or not
-        m_variable = *current;
-        // std::string temp(m_variable);
-        m_params.emplace_back("x");
-        current++;
-        next++;
-      } else if ((*current) == '+') {
-        m_params.emplace_back("+");
-        current++;
-        next++;
-      } else if ((*current) == '-') {
-        m_params.emplace_back("-");
-        current++;
-        next++;
-      } else if ((*current) == '*') {
-        m_params.emplace_back("*");
-        current++;
-        next++;
-      } else if ((*current) == '/') {
-        m_params.emplace_back("/");
-        current++;
-        next++;
+
+  void lexer(){
+   // trim spaces from the beginning
+   m_function_str.erase(m_function_str.begin(), std::find_if(m_function_str.begin(), m_function_str.end(), [](char ch){
+      return !std::isspace(ch);			   
+    })); 
+   // trim spaces from the end 
+   m_function_str.erase(std::find_if(m_function_str.rbegin(), m_function_str.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), m_function_str.end());
+   for(const auto& elt: m_function_str){
+      // ignore whitespaces	   
+      if(std::isspace(elt)){
+	 m_lookup_table.insert({elt, 3});     
       }
-    }
-    m_function = [&](int x) -> double {
-      double result = 0;
-      char current_op = '*';
-      bool no_op_read = true;
-      for (const auto &elt : m_params) {
-        if (isdigit(elt[0])) {
-          if (no_op_read)
-            result = std::stoi(elt);
-          else {
-            result = perform_operation(result, std::stoi(elt), current_op);
-          }
-        } else if (elt == "+") {
-          current_op = '+';
-          no_op_read = false;
-        } else if (elt == "-") {
-          no_op_read = false;
-          current_op = '-';
-        } else if (elt == "/") {
-          no_op_read = false;
-          current_op = '/';
-        } else if (elt == "*") {
-          no_op_read = false;
-          current_op = '*';
-        } else if (isalpha(elt[0])) {
-          if (no_op_read)
-            result = 1;
-          result = perform_operation(result, x, current_op);
-        }
+      else if(std::isdigit(elt)){
+	 m_lookup_table.insert({elt, 1});    
       }
-      return result;
-    };
+      else if(std::isalpha(elt)){
+	 m_lookup_table.insert({elt, 0});     
+      }
+      else if(IsOperator(elt)){
+	 m_lookup_table.insert({elt, 2});     
+      }
+   }
+  }
+
+   void parse(){
+     
+   }
+
+  void assign_lamdda(std::string str) {
+    	  
   }
 
   double Evaluate(int input) { return m_function(input); }
